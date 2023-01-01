b3764059a22a1619b2129f9a063e9ca7924bfde7
yemkwaemmanuel <edisonemmanuel21@gmail.com>
2023-01-01 15:02:13 +0100

    api backend planning & cours : implementation des routes et controllers

--- Backend/API/Routes/cours.js
+++ Backend/API/Routes/cours.js
@@ -3,4 +3,10 @@ const router = express.Router()
 
 const CoursController = require('../Controllers/CoursController')
 
+router.post('/store', CoursController.store)
+router.get ('/', CoursController.showAll)
+router.get ('/showByName', CoursController.showByName)
+router.post ('/update', CoursController.update)
+router.post('/delete',CoursController.destroy)
+
 module.exports = router
