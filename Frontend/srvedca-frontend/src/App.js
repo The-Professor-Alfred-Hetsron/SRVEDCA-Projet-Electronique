ade7e998ea12b74913c283807835d72c82c941cc
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-01-10 13:41:09 +0100

    Merge du frontend de Mevongo

---
+++ Frontend/srvedca-frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css';
+import { Home, Enrol, Course, Class, Admin, Planning, Presence } from './Routes'
+import { Routes, Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/accueil' element={<Home/>}/>
+        <Route path='/etudiant' element = {<Enrol/>}/>
+        <Route path='/classe' element = {<Class/>}/>
+        <Route path='/cours' element = {<Course/>}/>
+        <Route path='/admin' element = {<Admin/>}/>
+        <Route path='/planning' element = {<Planning/>}/>
+        <Route path='/présence' element = {<Presence/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
